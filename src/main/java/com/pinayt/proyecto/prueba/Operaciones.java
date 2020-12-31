14e326fa10d4edac1d8dddc7c2487aff4bf219a8
PinaYT <pinacomandos@gmail.com>
2020-12-31 17:54:31 -0300

    Calculadora
    Mi primera calculadora

---
+++ src/main/java/com/pinayt/proyecto/prueba/Operaciones.java
@@ -0,0 +1,19 @@
+
+package com.pinayt.proyecto.prueba;
+
+public class Operaciones {
+    public double result;
+    
+    public void calsuma(double n1, double n2){
+        result = n1 + n2;
+    }
+    public void calresta(double n1, double n2){
+        result = n1 - n2;
+    }
+    public void caldividir(double n1, double n2){
+        result = n1 / n2;
+    }
+    public void calmultiplicar(double n1, double n2){
+        result = n1 * n2;
+    }
+}
